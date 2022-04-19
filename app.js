c2d2ac3b50471e8b7e79c297fec96b1ec88e187c
André Díaz <andre290800@gmail.com>
2022-04-19 15:30:21 -0500

    explorersInNode route for returning objects

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.get("/launchx", (req, res) => {
   res.send("Bienvenidos a Launch X");
 });
 
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get("/explorersInNode", (req, res) => {
+  const explorer = { name: "Explorer", msg: "Hello" };
+  res.send(explorer);
+});
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
